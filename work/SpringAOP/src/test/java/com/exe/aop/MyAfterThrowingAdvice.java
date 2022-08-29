ad315bb5c41a08a193d3ce18faf8c39539df76c2
kimyoungwoon <ingngnin@gmail.com>
2022-08-29 20:59:07 +0900

    2022-08-29

---
+++ work/SpringAOP/src/test/java/com/exe/aop/MyAfterThrowingAdvice.java
@@ -0,0 +1,15 @@
+package com.exe.aop;
+
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyAfterThrowingAdvice {
+	
+	@AfterThrowing("execution(public void com..aop.*.*(..))")
+	public void afterThrowAdvice() {
+		System.out.println("Throw");
+	}
+}
---
+++ work/SpringAOP/src/test/java/com/exe/aop/MyAroundAdvice.java
@@ -0,0 +1,29 @@
+package com.exe.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class MyAroundAdvice {
+
+	@Around("execution(public void com..aop.*.*(..))")
+	public Object aroundMethodCall(ProceedingJoinPoint jointPoint) {
+		Object result = null;
+		
+		try {
+			System.out.println("메소드 실행 전(Around Before)");
+			result = jointPoint.proceed();
+			System.out.println("메소드 실행 후(Around After)");
+			
+		} catch (Throwable e) {
+			// TODO: handle exception
+		}
+		
+		return result;
+	}
+	
+}
